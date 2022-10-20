b906c3e4c554e327792afce2a056bd1092d737ec
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-10-21 00:11:17 +0530

    Add NewsApp API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,21 +2,22 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
     static propTypes = {}
 
     render() {
 
-        let {title, description, imageurl} = this.props;
+        let {title, description, imageUrl, newsUrl} = this.props;
 
         return (
             <>
                 <div>
-                    <div className="card" style={{width: "18rem"}}>
-                        <img src="https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg" className="card-img-top" alt="News img" />
+                    <div className="card" style={{width: "20rem", height: "500px", width: "auto"}}>
+                        <img src={imageUrl} className="card-img-top" alt="News img" />
                         <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href="/NewsDetail" className="btn btn-primary">Click to know more...</a>
+                            <h5 className="card-title">{title.slice(0, 70)}...</h5>
+                            <p className="card-text">{description.slice(0, 88)}...</p>
+                            <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read more..</a>
                         </div>
                     </div>
                 </div>
