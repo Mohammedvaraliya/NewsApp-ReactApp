09cd89c459418c903150bbf53a922bd380f7fcae
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-10-22 21:01:39 +0530

    Add NewsApp more categories

--- src/App.js
+++ src/App.js
@@ -6,20 +6,32 @@ import News from './components/News';
 import NewsItem from './components/NewsItem';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  Link
+  Switch,
 } from "react-router-dom";
 
 export default class App extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar/>
-        <News pageSize={9} country={"in"} category={"science"}/>
+      <>
+        <div>
+          <Router>
+            <Navbar />
+            <Switch>
+              <Route exact strict path="/" ><News key={'general'} pageSize={9} country={"in"} category={"general"} /></Route>
+              <Route exact strict path="/business"><News key={'business'} pageSize={9} country={"in"} category={"business"} /></Route>
+              <Route exact strict path="/entertainment"><News key={'entertainment'} pageSize={9} country="in" category={"entertainment"} /></Route>
+              <Route exact strict path="/general"><News key={'general'} pageSize={9} country="in" category={"general"} /></Route>
+              <Route exact strict path="/health"><News key={'health'} pageSize={9} country="in" category={"health"} /></Route>
+              <Route exact strict path="/science"><News key={'science'} pageSize={9} country="in" category={"science"} /></Route>
+              <Route exact strict path="/sports"><News key={'sports'} pageSize={9} country="in" category={"sports"} /></Route>
+              <Route exact strict path="/technology"><News key={'technology'} pageSize={9} country="in" category={"technology"} /></Route>
+            </Switch>
+          </Router>
         </div>
+      </>
     )
   }
 }
-
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export class Navbar extends Component {
 
@@ -12,18 +11,15 @@ export class Navbar extends Component {
             <>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">NewsDaddy</a>
+                        <Link className="navbar-brand" to="/">NewsDaddy</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <NavLink className={`nav-link ${"active"}`} aria-current="page" to="/home">Home</NavLink>
+                                    <Link className={`nav-link ${"active"}`} aria-current="page" to="/">Home</Link>
                                 </li>
-                                {/* <li className="nav-item">
-                                    <Link className={`nav-link ${"active"}`} to="/about">About</Link>
-                                </li> */}
                                 <li className="nav-item">
                                     <Link className={`nav-link ${"active"}`} to="/business">Business</Link>
                                 </li>
