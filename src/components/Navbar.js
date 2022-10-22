700b99dedeeffbf29f425a306fc8c8e29cb08019
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-10-22 19:04:22 +0530

    Add NewsApp more categories

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 export class Navbar extends Component {
+
     static propTypes = {}
 
     render() {
@@ -16,12 +19,33 @@ export class Navbar extends Component {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/home">Home</a>
+                                    <NavLink className={`nav-link ${"active"}`} aria-current="page" to="/home">Home</NavLink>
+                                </li>
+                                {/* <li className="nav-item">
+                                    <Link className={`nav-link ${"active"}`} to="/about">About</Link>
+                                </li> */}
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${"active"}`} to="/business">Business</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
+                                    <Link className={`nav-link ${"active"}`} to="/entertainment">Entertainment</Link>
                                 </li>
-                                
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${"active"}`} to="/general">General</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${"active"}`} to="/health">Health</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${"active"}`} to="/science">Science</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${"active"}`} to="/sports">Sports</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${"active"}`} to="/technology">technology</Link>
+                                </li>
+
                             </ul>
                         </div>
                     </div>
